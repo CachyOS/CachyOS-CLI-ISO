807774f59eb00e3f5c5a3e8755c53c4543d9d9db
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-06-21 02:52:00 +0300

    add restart plasma after autostart script is done.

--- airootfs/etc/skel/.local/bin/set-cachy-theme.sh
+++ airootfs/etc/skel/.local/bin/set-cachy-theme.sh
@@ -11,8 +11,12 @@ mv ./konsolerc ~/.config/
 # move ksplashrc
 mv ./ksplashrc ~/.config/
 
-
 # cleanup
 rm ~/.config/autostart/cachyos-theme.desktop
+
+# restart plasma
+kquitapp5 plasmashell || killall plasmashell && kstart5 plasmashell
+
+# remove this script
 rm ~/.local/bin/set-cachy-theme.sh
 
