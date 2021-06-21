29b20fdd3987825a3d2a71ea4d57576ce8b360ec
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-06-21 23:10:00 +0300

    iso name: cachyos-semi-offline

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="cachyos"
+iso_name="cachyos-semi-offline"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="CachyOS <https://cachyos.org>"
 iso_application="CachyOS Live/Rescue CD"
