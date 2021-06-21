60af1e2e965b46a3ce972a4799b422af651c3598
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-06-21 18:47:43 +0300

    move local db files up one folder

--- setup-offline-repo.sh
+++ setup-offline-repo.sh
@@ -1,31 +1,20 @@
 #!/usr/bin/env bash 
 
-# arch offline repo
-arch_offline_repo_path="./airootfs/root/offline-repos/arch-repo/"
+# offline repo
+offline_repo_path="./airootfs/root/offline-repos/"
+
+# packages
 arch_packages=" base base-devel linux linux-firmware systemd efibootmgr mkinitcpio vim"
+cachyos_packages=" cachyos-installer"
 
-# cachyos offline repo
-cachyos_local_git_repo_patch="/home/hamad/dev/cachyos/cachyos_repo"
-cachyos_offline_repo_path="./airootfs/root/offline-repos/cachyos-repo/"
-
-mkdir -p $arch_offline_repo_path
-mkdir -p $cachyos_offline_repo_path
+mkdir -p $offline_repo_path
 
 #
-# copy cachyos db and packages files
-cd $cachyos_local_git_repo_patch
-echo "**** git pull cachyos repo last update ****"
-git pull origin master
+# download arch and cachyos packages
+sudo pacman -Syw --cachedir $offline_repo_path \
+	--dbpath $offline_repo_path \
+	$arch_packages \
+	$cachyos_packages
 
-# cd back to this dir
-cd -
-echo "**** copy cachyos repo files ****"
-rsync -av --progress $cachyos_local_git_repo_patch/* $cachyos_offline_repo_path \
-	--exclude .git
-
-#
-# download arch packages
-sudo pacman -Syw --cachedir $arch_offline_repo_path \
-	--dbpath $arch_offline_repo_path \
-	$arch_packages
-
+# move .dbs up folder to be alongside with their packages
+mv $offline_repo_path/sync/* $offline_repo_path/
