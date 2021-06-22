f2e3f4cc213d220a2c05d26698c8f1996c4afdc0
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-06-23 00:50:48 +0300

    add more packages as full plasma to be ready to be installed offline also pipewire stuff since it is most likely will be installed

--- setup-offline-repo.sh
+++ setup-offline-repo.sh
@@ -3,17 +3,24 @@
 # offline repo
 offline_repo_path="./airootfs/root/offline-repos/"
 
+# comment this out if you need to confirm
+noconfirm="--noconfirm"
+
 # packages
 arch_packages=" base base-devel linux linux-firmware systemd \
 		efibootmgr mkinitcpio vim networkmanager grub \
-		libinih libedit ntp xfsprogs"
-cachyos_packages=" linux-cacule-headers linux-cacule cachyos-installer"
+		libinih libedit ntp xfsprogs \
+		plasma-meta kde-applications-meta xorg \
+		pipewire pipewire-alsa pipewire-jack \
+		pipewire-media-session pipewire-pulse \
+		gst-plugin-pipewire libpulse"
+cachyos_packages=" linux-cacule-headers linux-cacule cachyos-kde-theme"
 
 mkdir -p $offline_repo_path
 
 #
 # download arch and cachyos packages
-sudo pacman -Syw --cachedir $offline_repo_path \
+sudo pacman $noconfirm -Syw --cachedir $offline_repo_path \
 	--dbpath $offline_repo_path \
 	$arch_packages \
 	$cachyos_packages
