0d4b30fe160fd97da6bdbc76e11ec78bb9bb9a6a
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-06-25 07:12:01 +0300

    - dolphin settings - open sans font

--- airootfs/etc/skel/.local/bin/set-cachy-theme.sh
+++ airootfs/etc/skel/.local/bin/set-cachy-theme.sh
@@ -5,11 +5,14 @@ sleep 7s
 # set the theme for the first time
 lookandfeeltool --resetLayout -a CachyOS
 
-# move konsolerc
-mv ./konsolerc ~/.config/
-
-# move ksplashrc
-mv ./ksplashrc ~/.config/
+# set font settings
+sed -i 's/\[General\]/[General]\
+XftAntialias=true\
+XftHintStyle=hintfull\
+font=Open Sans,10,-1,5,50,0,0,0,0,0\
+menuFont=Open Sans,10,-1,5,50,0,0,0,0,0\
+smallestReadableFont=Open Sans,8,-1,5,50,0,0,0,0,0\
+toolBarFont=Open Sans,10,-1,5,50,0,0,0,0,0/g' ~/.config/kdeglobals
 
 # cleanup
 rm ~/.config/autostart/cachyos-theme.desktop
