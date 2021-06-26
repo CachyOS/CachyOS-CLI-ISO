836c4a10760929a735c97e36e48a971e5c7af019
Hamad Al Marri <hamad@cachyos.org>
2021-06-26 10:04:34 +0300

    cleanup

--- setup-offline-repo.sh
+++ setup-offline-repo.sh
@@ -7,14 +7,14 @@ offline_repo_path="./airootfs/root/offline-repos/"
 noconfirm="--noconfirm"
 
 # packages
-arch_packages=" base base-devel linux linux-firmware systemd \
-		efibootmgr mkinitcpio vim networkmanager grub \
+base_packages="base base-devel linux linux-firmware mkinitcpio systemd vim"
+arch_packages="efibootmgr mkinitcpio networkmanager grub \
 		libinih libedit ntp xfsprogs \
 		plasma-meta kde-applications-meta xorg \
 		pipewire pipewire-alsa pipewire-jack \
 		pipewire-media-session pipewire-pulse \
 		gst-plugin-pipewire libpulse"
-cachyos_packages=" linux-cachyos-headers linux-cachyos cachyos-kde-theme"
+cachyos_packages="linux-cachyos-headers linux-cachyos cachyos-kde-theme"
 
 mkdir -p $offline_repo_path
 
@@ -22,6 +22,7 @@ mkdir -p $offline_repo_path
 # download arch and cachyos packages
 sudo pacman $noconfirm -Syw --cachedir $offline_repo_path \
 	--dbpath $offline_repo_path \
+	$base_packages \
 	$arch_packages \
 	$cachyos_packages
 
