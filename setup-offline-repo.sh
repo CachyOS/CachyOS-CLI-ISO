9ed804f25fce96ef570ba9960a52da490e709f76
Hamad Al Marri <hamad@cachyos.org>
2021-07-10 21:27:02 +0300

    add x86_64_v3 to offline repo setup

--- setup-offline-repo.sh
+++ setup-offline-repo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 # offline repo
-offline_repo_path="./airootfs/root/offline-repos/"
+offline_repo_path="./airootfs/root/offline-repos"
 
 # comment this out if you need to confirm
 noconfirm="--noconfirm"
@@ -15,19 +15,28 @@ arch_packages="efibootmgr mkinitcpio networkmanager grub \
 		pipewire pipewire-alsa pipewire-jack \
 		pipewire-media-session pipewire-pulse \
 		gst-plugin-pipewire libpulse"
-cachyos_packages="linux-cachyos-headers linux-cachyos cachyos-kde-theme"
+cachyos_packages="cachyos/linux-cachyos-headers cachyos/linux-cachyos cachyos/cachyos-kde-theme"
+cachyos_x86_64_v3_packages="cachyos-x86-64-v3/linux-cachyos-headers cachyos-x86-64-v3/linux-cachyos"
 
-mkdir -p $offline_repo_path
+mkdir -p $offline_repo_path/
+mkdir -p $offline_repo_path/x86_64_v3/
+
 # move previous .dbs to sync folder
 mv $offline_repo_path/*.db* $offline_repo_path/sync/
+mv $offline_repo_path/x86_64_v3/*.db* $offline_repo_path/x86_64_v3/sync/
 
 #
 # download arch and cachyos packages
-pacman $noconfirm -Syw --cachedir $offline_repo_path \
-	--dbpath $offline_repo_path \
+pacman $noconfirm -Syw --cachedir $offline_repo_path/ \
+	--dbpath $offline_repo_path/ \
 	$base_packages \
 	$arch_packages \
 	$cachyos_packages
 
+pacman $noconfirm -Syw --cachedir $offline_repo_path/x86_64_v3/ \
+	--dbpath $offline_repo_path/x86_64_v3/ \
+	$cachyos_x86_64_v3_packages
+
 # move .dbs up folder to be alongside with their packages
 mv $offline_repo_path/sync/* $offline_repo_path/
+mv $offline_repo_path/x86_64_v3/sync/* $offline_repo_path/x86_64_v3/
