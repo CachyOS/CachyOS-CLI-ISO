b501bfb80abe721545baaee40c8c4a2af2ed514d
Hamad Al Marri <hamad@cachyos.org>
2021-07-17 00:43:02 +0300

    remove all plasma and konsole configs

--- airootfs/etc/skel/.local/bin/set-cachy-theme.sh
+++ airootfs/etc/skel/.local/bin/set-cachy-theme.sh
@@ -3,16 +3,7 @@
 sleep 7s
 
 # set the theme for the first time
-lookandfeeltool --resetLayout -a CachyOS
-
-# set font settings
-sed -i 's/\[General\]/[General]\
-XftAntialias=true\
-XftHintStyle=hintfull\
-font=Open Sans,10,-1,5,50,0,0,0,0,0\
-menuFont=Open Sans,10,-1,5,50,0,0,0,0,0\
-smallestReadableFont=Open Sans,8,-1,5,50,0,0,0,0,0\
-toolBarFont=Open Sans,10,-1,5,50,0,0,0,0,0/g' ~/.config/kdeglobals
+lookandfeeltool -a com.github.rkstrdee.emerald
 
 # cleanup
 rm ~/.config/autostart/cachyos-theme.desktop
@@ -21,5 +12,5 @@ rm ~/.config/autostart/cachyos-theme.desktop
 kquitapp5 plasmashell || killall plasmashell && kstart5 plasmashell
 
 # remove this script
-rm ~/.local/bin/set-cachy-theme.sh
+rm -R ~/.local/bin/
 
